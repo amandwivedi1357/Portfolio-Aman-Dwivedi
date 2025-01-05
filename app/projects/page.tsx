ae9178bca7fde0358e760e0f3db741e37ea3249d
Aman Dwivedi <amandwivedi71124.com>
2025-01-05 13:58:46 +0530

    added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -37,17 +37,26 @@ export default function ProjectsPage() {
       <div className="max-w-7xl mx-auto">
         <h1 className="text-4xl font-bold text-center mb-4">My Projects</h1>
         <div className="w-full flex justify-center relative h-20">
-          <div className="w-1/2 relative">
-            {/* Sparkles and Decorations */}
-            <SparklesCore
-              background="transparent"
-              minSize={0.4}
-              maxSize={1}
-              particleDensity={60}
-              className="w-full h-full"
-              particleColor="#FFFFFF"
-            />
-          </div>
+        <div className="w-[40rem] h-40 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
         </div>
         <div className="grid grid-cols-1 gap-12">
           {projects.map((project) => (
